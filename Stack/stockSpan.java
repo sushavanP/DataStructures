dcaeed92a8628c5c9607de952a665fd84666834b
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-06-16 14:28:21 +0530

    Add files via upload

---
+++ Stack/stockSpan.java
@@ -0,0 +1,28 @@
+package dataStructures.Stack;
+import java.util.Stack;
+import java.util.Arrays;
+
+public class stockSpan
+{
+    static void calculateSpan(int price[], int n, int s[]){
+        Stack <Integer> st = new Stack<>();
+        st.push(0);
+        s[0] = 1;
+        for(int i = 1; i < n; i++){
+            while(!st.empty() && price[st.peek()] <= price[i])
+                st.pop();
+            s[i] = (st.empty()) ? (i + 1) : (i - st.peek());
+            st.push(i);
+        }
+    }
+    static void printArray(int arr[]){
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args){
+        int price[] = {10, 4, 5, 90, 120, 80};
+        int n = price.length;
+        int s[] = new int[n];
+        calculateSpan(price, n, s);
+        printArray(s);
+    }
+}
